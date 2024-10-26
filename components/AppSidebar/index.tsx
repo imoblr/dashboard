aea9b8d4b92e763cd6a24631462afa10e2afc796
Kaue Machado <kaumac@gmail.com>
2024-10-26 16:03:07 -0300

    wip

--- components/AppSidebar/index.tsx
+++ components/AppSidebar/index.tsx
@@ -4,17 +4,20 @@ import CollapseLeftIcon from "@/assets/icons/collapse-left.svg";
 import CollapseRightIcon from "@/assets/icons/collapse-right.svg";
 
 import SidebarMenu from "./SidebarMenu";
+import { Pressable } from "react-native";
 
 const AppSidebar = () => {
 	return (
 		<Box className="w-[64px] h-full overflow-visible z-50">
-			<Box className="h-full w-[240px] bg-background border-r-[1px] border-border-lighter p-2 pt-4">
-				<Center className="h-[32px] w-[32px] absolute top-10 right-[-16px] z-50 bg-background rounded-full border border-border-lighter shadow-xs">
-					<Icon
-						as={CollapseLeftIcon}
-						className="text-text-quaternary w-[18px] h-[18px]"
-					/>
-				</Center>
+			<Box className="h-full w-[240px] bg-background border-r-[1px] border-border-lighter p-2 pt-4 shadow-xs">
+				<Pressable className="absolute top-8 right-[-16px] z-50">
+					<Center className="h-[32px] w-[32px] hover:bg-primary-25 bg-background rounded-full border border-border shadow-xs">
+						<Icon
+							as={CollapseLeftIcon}
+							className="text-text-tertiary w-[18px] h-[18px]"
+						/>
+					</Center>
+				</Pressable>
 				<Box className="pl-2">
 					<ImoblrSidebarLogo width={100} height={28} />
 				</Box>
