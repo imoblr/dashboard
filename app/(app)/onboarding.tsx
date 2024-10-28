836d6a35ac160220286cf9c97c38db2f5df3cfec
Kaue Machado <kaumac@gmail.com>
2024-10-28 16:56:36 -0300

    update color shades

---
+++ app/(app)/onboarding.tsx
@@ -0,0 +1,13 @@
+import { Stack, Link } from "expo-router";
+import { Box, Text } from "@/components/ui";
+
+export default function OnboardingPage() {
+	return (
+		<Box className="flex-1 overflow-hidden">
+			<Text>
+				opkfsdfko sdogsdogdfskogsd asgoadsfgokasdfgk
+				asgoadsfgokasdfgkgadsfogdfsakogs
+			</Text>
+		</Box>
+	);
+}
